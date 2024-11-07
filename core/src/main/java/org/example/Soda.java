cf6eecd85fa0ff2dd342d085a394f19c6ca0471b
Jim Bowring <bowring@gmail.com>
2024-11-07 15:46:10 -0500

    Revert "tweaks to get it running (#1)" (#2)
    This reverts commit 0db913fdb32f59de817af624c91ed366a3e6ba35.

--- core/src/main/java/org/example/Soda.java
+++ core/src/main/java/org/example/Soda.java
@@ -87,7 +87,7 @@ public class Soda implements Beverage, Serializable, Comparable<Soda> {
     public static Soda deserializeFromCSV(String filename) throws IOException {
         Path path = Paths.get(filename);
         List<String> filedata = Files.readAllLines(path);
-        String[] data = filedata.get(0).split(",");
+        String[] data = filedata.getFirst().split(",");
         Files.deleteIfExists(path);
         return formatValues(data);
     }
